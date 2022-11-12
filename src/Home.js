5e3ea60bbde044ebaf0e9fa8c401e8c63a578683
Sudipa Acharya <sudipaacharya351@gmail.com>
2022-11-13 00:28:07 +0530

    pushcode

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Home.css";
+
+function Home(){
+    return (
+        <div className="home">
+            <img className="homke_image" src="../front image.jpg" width="2020" height="700" alt=""/>
+        </div>
+    );
+}
+
+export default Home;
